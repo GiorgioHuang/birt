2e5537c4ebde3160f8aa704c80e186cf6c7652ba
Yueqian Wang <ywang@actuate.com>
2010-04-13 03:31:21 +0000

    - Summary: Add new methods to calculate width of table to 2.3.2 Branch
    -Bug (s) Resolved:
    
    - Description:
    Add a new method setWidthToFitColumns() to TableHandle.
    It  calculates the sum of all table columns' width and set it to table if possible.
    Several exceptions will be thrown out if the width cannot be calculated.
    
    Add a new method mergeDimension(DimensionValue,DimensionValue) to DimensionUtil to help table width's calculation.
    
    Add a new method convertWidthToAbsoluteValue() to ColumnHandle
    It converts column's width from percentage to an absolute value if possible, otherwise it will do nothing.
    
    Add new unit tests for new methods.
    
    Update table column related Javadocs to make them more clearly.
    
    - Tests Description:
    Auto Test
    
    - Files Edited:
    
    - Files Added:
    
    - Files Deleted:
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/elements/ColumnHelper.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/elements/ColumnHelper.java
@@ -11,6 +11,9 @@
 
 package org.eclipse.birt.report.model.elements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.birt.report.model.core.ContainerSlot;
 import org.eclipse.birt.report.model.core.Module;
 import org.eclipse.birt.report.model.elements.interfaces.ITableColumnModel;
@@ -18,7 +21,7 @@ import org.eclipse.birt.report.model.elements.interfaces.ITableColumnModel;
 /**
  * Provides methods for style property values on the column. Currently, only
  * TableItem and GridItem have columns.
- *  
+ * 
  */
 
 public final class ColumnHelper
@@ -31,33 +34,105 @@ public final class ColumnHelper
 	 *            the report design
 	 * @param columnSlot
 	 *            the slot contains columns
-	 * @param columnNum
-	 *            the column number to search
+	 * @param columnIndex
+	 *            the 1-based column index
 	 * 
-	 * @return the index of a column.
+	 * @return the column at the specified position in the slot, or null if not
+	 *         found.
 	 */
 
 	public static TableColumn findColumn( Module module,
-			ContainerSlot columnSlot, int columnNum )
+			ContainerSlot columnSlot, int columnIndex )
 	{
-		assert columnNum > 0;
+		assert columnIndex > 0;
 
 		for ( int i = 0, index = 0; i < columnSlot.getCount( ); i++ )
 		{
 			TableColumn column = (TableColumn) ( columnSlot.getContent( i ) );
-			int repeat = column
-					.getIntProperty( module, ITableColumnModel.REPEAT_PROP );
 
-			// in default, repeat is one.
+			index += getColumnRepeat( module, column );
 
-			repeat = ( repeat == 0 ) ? 1 : repeat;
-
-			index += repeat;
-
-			if ( index >= columnNum )
+			if ( index >= columnIndex )
 				return column;
 		}
 
 		return null;
+	}
+
+	/**
+	 * Gets tableColumns in the grid or table, if the column repeats it will be
+	 * recorded accordingly.
+	 * 
+	 * @param module
+	 *            the module
+	 * @param columnSlot
+	 *            the column slot.
+	 * @return an array containing TableColumn.
+	 */
+	public static TableColumn[] getTableColumnArray( Module module,
+			ContainerSlot columnSlot )
+	{
+		int colNum = columnSlot.getCount( );
+
+		List<TableColumn> tmpColumns = new ArrayList<TableColumn>( );
+
+		for ( int i = 0; i < colNum; i++ )
+		{
+			TableColumn tmpColumn = (TableColumn) columnSlot.getContent( i );
+			int repeat = getColumnRepeat( module, tmpColumn );
+
+			for ( int j = 0; j < repeat; j++ )
+			{
+				tmpColumns.add( tmpColumn );
+			}
+		}
+
+		return tmpColumns.toArray( new TableColumn[0] );
+	}
+
+	/**
+	 * Gets the column according to the column number.
+	 * 
+	 * @param columnArray
+	 *            the column array
+	 * @param columnNum
+	 *            the column number
+	 * @return the table column.
+	 */
+	public static TableColumn getColumnInArray( TableColumn[] columnArray,
+			int columnNum )
+	{
+		if ( columnArray == null )
+			return null;
+
+		// the column number start from 1, so it should be subtracted 1.
+		// if the column number found by cell is lager than the length of
+		// column, table has inconsistent column count so return null
+		int colIndex = columnNum - 1;
+		if ( colIndex < columnArray.length )
+		{
+			return columnArray[colIndex];
+		}
+		return null;
+	}
+
+	/**
+	 * Gets the repeat times of the column.
+	 * 
+	 * @param module
+	 *            the module.
+	 * @param column
+	 *            the column.
+	 * @return the column length.
+	 */
+	private static int getColumnRepeat( Module module, TableColumn column )
+	{
+		int repeat = column.getIntProperty( module,
+				ITableColumnModel.REPEAT_PROP );
+
+		// in default, repeat is one.
+
+		repeat = ( repeat <= 0 ) ? 1 : repeat;
+		return repeat;
 	}
 }
