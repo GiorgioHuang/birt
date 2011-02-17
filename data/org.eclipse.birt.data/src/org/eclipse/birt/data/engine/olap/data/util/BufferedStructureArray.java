35747e8b9f8911d632070a94dc910dcf92d32c20
xiazhang <>
2011-02-17 02:11:35 +0000

    Fix a memory leak bug.Some SciptContext objects have not be closed correctly.[37489]

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/olap/data/util/BufferedStructureArray.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/olap/data/util/BufferedStructureArray.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.eclipse.birt.data.engine.impl.DataEngineThreadLocal;
+import org.eclipse.birt.data.engine.api.DataEngineThreadLocal;
 
 /**
  * 
