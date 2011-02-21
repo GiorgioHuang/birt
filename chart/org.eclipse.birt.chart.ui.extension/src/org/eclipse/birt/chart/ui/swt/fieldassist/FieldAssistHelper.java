6242925599c49da01c8764ca942d64424cc82236
heli <>
2011-02-21 07:07:11 +0000

    - Summary: [BZ]NLS unused/missing messages
     - Bugzilla Bug (s) Resolved: 37730
    
    - Description:
    Fixed by changing the method for getting keys
    
    - Tests Description:  Manual Test
    
    - Notes to Build Team: N/A
    
    - Notes to Developers: N/A
    
    - Notes to QA:  N/A
    
    - Notes to Documentation:  N/A
    
    - Files Edited:
    
    
    - Files Added:
    
    
    - Files Deleted

--- chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/fieldassist/FieldAssistHelper.java
+++ chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/fieldassist/FieldAssistHelper.java
@@ -11,6 +11,7 @@
 
 package org.eclipse.birt.chart.ui.swt.fieldassist;
 
+import org.eclipse.birt.chart.ui.extension.i18n.Messages;
 import org.eclipse.birt.chart.ui.plugin.ChartUIExtensionPlugin;
 import org.eclipse.birt.chart.ui.swt.fieldassist.preferences.FieldAssistPreferenceInitializer;
 import org.eclipse.birt.chart.ui.swt.fieldassist.preferences.PreferenceConstants;
@@ -81,11 +82,11 @@ public final class FieldAssistHelper
 		{
 			if ( ChartUIExtensionPlugin.getDefault( ) == null )
 			{
-				soPreferenceStore = new PreferenceStore();
+				soPreferenceStore = new PreferenceStore( );
 				FieldAssistPreferenceInitializer.setDefaultPreferences( soPreferenceStore );
 				return;
 			}
-			
+
 			soPreferenceStore = ChartUIExtensionPlugin.getDefault( )
 					.getPreferenceStore( );
 		}
@@ -109,7 +110,7 @@ public final class FieldAssistHelper
 		{
 			return getPreferenceStore( ).getString( PreferenceConstants.PREF_CONTENTASSISTKEYCUSTOMKEY );
 		}
-		
+
 		return triggerKey;
 	}
 
@@ -126,10 +127,10 @@ public final class FieldAssistHelper
 		{
 			return getPreferenceStore( ).getString( PreferenceConstants.PREF_CONTENTASSISTKEYCUSTOMKEY );
 		}
-		
+
 		return triggerKey;
 	}
-	
+
 	private int getMarginWidth( )
 	{
 		IPreferenceStore store = getPreferenceStore( );
@@ -351,7 +352,7 @@ public final class FieldAssistHelper
 	{
 		Menu newMenu = new Menu( field.control );
 		MenuItem item = new MenuItem( newMenu, SWT.PUSH );
-		item.setText( FieldAssistMessages.ssDecorationMenuItem );
+		item.setText( Messages.getString( "ssDecorationMenuItem" ) ); //$NON-NLS-1$
 		item.addSelectionListener( new SelectionListener( ) {
 
 			public void widgetSelected( SelectionEvent event )
@@ -484,14 +485,14 @@ public final class FieldAssistHelper
 			// text.
 			FieldDecoration standardDecoration = registry.getFieldDecoration( FieldDecorationRegistry.DEC_CONTENT_PROPOSAL );
 			registry.registerFieldDecoration( DEC_CONTENTASSIST_ID,
-					NLS.bind( FieldAssistMessages.ssDecoratorContentAssist,
+					NLS.bind( Messages.getString( "ssDecoratorContentAssist" ), //$NON-NLS-1$
 							getTriggerKeyText( ) ),
 					standardDecoration.getImage( ) );
 			dec = registry.getFieldDecoration( DEC_CONTENTASSIST_ID );
 		}
 		else
 		{
-			dec.setDescription( NLS.bind( FieldAssistMessages.ssDecoratorContentAssist,
+			dec.setDescription( NLS.bind( Messages.getString( "ssDecoratorContentAssist" ), //$NON-NLS-1$
 					getTriggerKeyText( ) ) );
 		}
 		return dec;
@@ -523,7 +524,7 @@ public final class FieldAssistHelper
 		gl.horizontalSpacing = 0;
 		gl.verticalSpacing = 0;
 	}
-	
+
 	/**
 	 * Add a required field indicator to related label component.
 	 * 
@@ -532,15 +533,16 @@ public final class FieldAssistHelper
 	 */
 	public void addRequiredFieldIndicator( AssistField assistField, Label label )
 	{
-		boolean showRequiredFieldLabelIndicator = isShowingRequiredFieldIndicator();
-		if (showRequiredFieldLabelIndicator && assistField.isRequiredField()) {
-			String text = label.getText();
+		boolean showRequiredFieldLabelIndicator = isShowingRequiredFieldIndicator( );
+		if ( showRequiredFieldLabelIndicator && assistField.isRequiredField( ) )
+		{
+			String text = label.getText( );
 			// This concatenation could be done by a field assist helper.
-			text = text.concat("*"); //$NON-NLS-1$
-			label.setText(text);
+			text = text.concat( "*" ); //$NON-NLS-1$
+			label.setText( text );
 		}
 	}
-	
+
 	/**
 	 * Add a required field indicator to related label component.
 	 * 
@@ -549,17 +551,18 @@ public final class FieldAssistHelper
 	 */
 	public void addRequiredFieldIndicator( Label label )
 	{
-		boolean showRequiredFieldLabelIndicator = isShowingRequiredFieldIndicator();
-		if (showRequiredFieldLabelIndicator ) {
-			String text = label.getText();
+		boolean showRequiredFieldLabelIndicator = isShowingRequiredFieldIndicator( );
+		if ( showRequiredFieldLabelIndicator )
+		{
+			String text = label.getText( );
 			// This concatenation could be done by a field assist helper.
-			text = text.concat("*"); //$NON-NLS-1$
-			label.setText(text);
+			text = text.concat( "*" ); //$NON-NLS-1$
+			label.setText( text );
 		}
 	}
-	
-	public boolean isShowingRequiredFieldIndicator()
+
+	public boolean isShowingRequiredFieldIndicator( )
 	{
-		return getPreferenceStore().getBoolean( PreferenceConstants.PREF_SHOWREQUIREDFIELDLABELINDICATOR );
+		return getPreferenceStore( ).getBoolean( PreferenceConstants.PREF_SHOWREQUIREDFIELDLABELINDICATOR );
 	}
 }
