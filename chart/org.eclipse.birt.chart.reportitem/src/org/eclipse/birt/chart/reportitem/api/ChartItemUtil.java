d286b471600d8730e394f748aae3f7ddac0517f8
heli <>
2011-05-11 05:25:24 +0000

    - Summary: exception occur if chart is from library and can't find library file.(#39136)
     - Bugzilla Bug (s) Resolved:
    
     - Description:
    This issue was caused by a previous change, the Null chart model is not checked when updating chart bounds. Now fixed.
    
    - Tests Description:  Manual Test
    
    - Notes to Build Team: N/A
    
    - Notes to Developers: N/A
    
    - Notes to QA:  N/A
    
    - Notes to Documentation:  N/A
    
    - Files Edited:
    
    
    - Files Added:
    
    
    - Files Deleted

--- chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/api/ChartItemUtil.java
+++ chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/api/ChartItemUtil.java
@@ -1317,6 +1317,10 @@ public class ChartItemUtil extends ChartExpressionUtil implements
 		{
 			ExtendedItemHandle hostChart = (ExtendedItemHandle) eih.getElementProperty( ChartReportItemConstants.PROPERTY_HOST_CHART );
 			cm = ChartCubeUtil.getChartFromHandle( hostChart );
+			if ( cm == null )
+			{
+				return null;
+			}
 			// Use plot chart's size as axis chart's. Even if model sizes
 			// are different, the output size are same
 			if ( ( (ChartWithAxes) cm ).isTransposed( ) )
@@ -1333,6 +1337,10 @@ public class ChartItemUtil extends ChartExpressionUtil implements
 		else
 		{
 			cm = ChartCubeUtil.getChartFromHandle( eih );
+			if ( cm == null )
+			{
+				return null;
+			}
 			dhHeight = eih.getHeight( );
 			dhWidth = eih.getWidth( );
 		}
