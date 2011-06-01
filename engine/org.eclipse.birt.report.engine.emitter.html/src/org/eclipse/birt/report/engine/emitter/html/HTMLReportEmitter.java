d92b29f2d178658e49bb218d5a3f96f29a050e2e
jili <>
2011-06-01 01:43:11 +0000

    40164 - Chart display inline does not work in Firefox 4

--- engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/HTMLReportEmitter.java
+++ engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/HTMLReportEmitter.java
@@ -2914,6 +2914,7 @@ public class HTMLReportEmitter extends ContentEmitterAdapter
 				writer.openTag( HTMLTags.TAG_MAP );
 				writer.attribute( HTMLTags.ATTR_ID, imageMapId );
 				writer.attribute( HTMLTags.ATTR_NAME, imageMapId );
+				writer.attribute( HTMLTags.ATTR_STYLE, "display:none" );//$NON-NLS-1$
 				writer.cdata( (String) imageMapObject );
 				writer.closeTag( HTMLTags.TAG_MAP );
 			}
