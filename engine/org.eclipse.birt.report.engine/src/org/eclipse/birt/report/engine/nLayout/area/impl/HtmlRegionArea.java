2b40dc3f2307eb9c0bee2685d7c7f63d07921745
Gang Liu <gliu@actuate.com>
2011-07-11 06:56:11 +0000

    Fix T41937: NPE when render report with large line height and letter spacing styles in item

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/nLayout/area/impl/HtmlRegionArea.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/nLayout/area/impl/HtmlRegionArea.java
@@ -37,7 +37,7 @@ public class HtmlRegionArea extends RegionArea implements IContainerArea
 			finished = false;
 		}
 		setContentHeight( specifiedHeight );
-		checkDisplayNone( );
+		//checkDisplayNone( );
 	}
 	
 	public void update( AbstractArea area ) throws BirtException
